84ca8c14ab6972de020b5d2e2a2937b0f98a0007
Ruomin Zhu <mcrzhu@gmail.com>
2019-10-23 15:15:31 +1100

    some test on forecast

--- Python/ASN/analysis/forecast_kevin.py
+++ Python/ASN/analysis/forecast_kevin.py
@@ -1,27 +1,13 @@
 import numpy as np
 from utils import *
 
-def expand_signal(simulationOptions, scale = 10, resting = True, resting_ratio = 0.5):
-    expand_length = int(scale*(1-resting_ratio))
-
-    simulationOptions.T *=  scale
-    simulationOptions.TimeVector = np.arange(0,simulationOptions.T, simulationOptions.dt)
-    simulationOptions.NumOfIterations *= scale
-    for this_stimulus in simulationOptions.stimulus:
-        temp = this_stimulus.signal[:]
-        this_stimulus.signal = np.zeros(simulationOptions.NumOfIterations)
-        count = 0
-        for i in range(len(temp)):
-            if resting:
-                this_stimulus.signal[count:count+expand_length] = temp[i]
-                this_stimulus.signal[count+expand_length:count+scale] = 0.005
-                count += scale
-            else:
-                this_stimulus.signal[count:count+scale] = temp[i]
-                count += scale
-    for i in range(1, len(simulationOptions.stimulus)):
-        simulationOptions.stimulus[i].signal = np.zeros(simulationOptions.TimeVector.size)
-    return simulationOptions
+def expander(some, scale = 10):
+    output = np.zeros(scale*len(some))
+    count = 0
+    for i in range(len(some)):
+        output[count:count+scale] = some[i]
+        count += scale
+    return output
 
 def pre_activation(sim_options, connectivity, junctionState):
     SimulationOptions = simulation_options__(dt = sim_options.dt, T = 1,
@@ -53,13 +39,11 @@ def getWeight(wireVoltage, stimulus, non_elec):
     weight = lsq_linear(lhs, rhs, (-10,10))['x']
     return weight
 
-def forecast(simulationOptions, connectivity, junctionState, training_ratio = 0.5, pre_activate = False):
+def forecast_kev(simulationOptions, connectivity, junctionState, training_ratio = 0.5, exclude_electrode = False, pre_activate = True):
     if simulationOptions.contactMode == 'farthest':
         simulationOptions.electrodes = get_farthest_pairing(connectivity.adj_matrix)
     elif simulationOptions.contactMode == 'Alon':
         simulationOptions.electrodes = get_boundary_pairing(connectivity, 28)
-    
-    simulationOptions = expand_signal(simulationOptions)
 
     niterations = simulationOptions.NumOfIterations
     training_length = int(niterations * training_ratio)
@@ -67,6 +51,11 @@ def forecast(simulationOptions, connectivity, junctionState, training_ratio = 0.
     numOfElectrodes = len(electrodes)
     E = connectivity.numOfJunctions
     V = connectivity.numOfWires    
+    if exclude_electrode:
+        non_elec = np.setdiff1d(np.arange(V), electrodes)
+    else:
+        non_elec = np.arange(V)
+    num_non_elec = len(non_elec)
 
     edgeList = connectivity.edge_list
     rhs = np.zeros(V+numOfElectrodes)
@@ -126,13 +115,61 @@ def forecast(simulationOptions, connectivity, junctionState, training_ratio = 0.
         Network.junctionVoltage[this_time,:] = junctionState.voltage
         Network.junctionResistance[this_time,:] = junctionState.resistance
         Network.junctionSwitch[this_time,:] = junctionState.OnOrOff
-    # weight = getWeight(Network.wireVoltage[:training_length,:], simulationOptions.stimulus[0].signal[:training_length], non_elec)
-    # # import matplotlib.pyplot as plt
-    # # plt.plot(weight)
-    # # plt.show()
-    # forecast = np.zeros(niterations)
-    # forecast[:training_length] = simulationOptions.stimulus[0].signal[:training_length]
-    
+
+    weight = getWeight(Network.wireVoltage[:training_length,:], simulationOptions.stimulus[0].signal[:training_length], non_elec)
+    # import matplotlib.pyplot as plt
+    # plt.plot(weight)
+    # plt.show()
+    forecast = np.zeros(niterations)
+    forecast[:training_length] = simulationOptions.stimulus[0].signal[:training_length]
+
+    for this_time in tqdm(range(training_length, niterations), desc='Forecasting '):
+        junctionState.updateResistance()
+        junctionConductance = 1/junctionState.resistance
+        
+        Gmat = np.zeros((V,V))
+        Gmat[edgeList[:,0], edgeList[:,1]] = junctionConductance
+        Gmat[edgeList[:,1], edgeList[:,0]] = junctionConductance
+        Gmat = np.diag(np.sum(Gmat, axis=0)) - Gmat
+
+        lhs = np.zeros((V+numOfElectrodes, V+numOfElectrodes))
+        lhs[0:V,0:V] = Gmat
+
+        last_V2 = Network.wireVoltage[this_time-2,non_elec]
+        last_V = Network.wireVoltage[this_time-1,non_elec]
+        hist = np.zeros(2*num_non_elec+2)
+        hist[0] = 1
+        hist[1] = forecast[this_time-1]
+        hist[2:num_non_elec+2] = last_V
+        hist[num_non_elec+2:] = last_V2
+        forecast[this_time] = np.dot(hist, weight)
+
+        for i in range(numOfElectrodes):
+            this_elec = electrodes[i]
+            lhs[V+i, this_elec] = 1
+            lhs[this_elec, V+i] = 1
+            if i == 0:
+                rhs[V+i] = forecast[this_time]
+        
+        # from scipy.sparse import csc_matrix
+        # from scipy.sparse.linalg import spsolve
+        # LHS = csc_matrix(lhs)
+        # RHS = csc_matrix(rhs.reshape(V+numOfElectrodes,1))
+        # sol = spsolve(LHS,RHS)
+
+        sol = np.linalg.solve(lhs,rhs)
+        wireVoltage = sol[0:V]
+        junctionState.voltage = wireVoltage[edgeList[:,0]] - wireVoltage[edgeList[:,1]]
+        junctionState.updateJunctionState(simulationOptions.dt)
+
+        Network.wireVoltage[this_time,:] = wireVoltage
+        Network.electrodeCurrent[this_time,:] = sol[V:]
+        Network.filamentState[this_time,:] = junctionState.filamentState
+        Network.junctionVoltage[this_time,:] = junctionState.voltage
+        Network.junctionResistance[this_time,:] = junctionState.resistance
+        Network.junctionSwitch[this_time,:] = junctionState.OnOrOff
+
+
     Network.numOfWires = V
     Network.numOfJunctions = E
     Network.adjMat = connectivity.adj_matrix
@@ -143,9 +180,42 @@ def forecast(simulationOptions, connectivity, junctionState, training_ratio = 0.
 
     Network.electrodes = simulationOptions.electrodes
     Network.criticalFlux = junctionState.critialFlux
-    # Network.stimulus = [simulationOptions.stimulus[i] for i in range(numOfElectrodes)]
-    Network.stimulus = simulationOptions.stimulus
+    Network.stimulus = [simulationOptions.stimulus[i] for i in range(numOfElectrodes)]
     # Network.junctionList = np.add(connectivity.edge_list, 1).T
     Network.connectivity = connectivity
     Network.TimeVector = simulationOptions.TimeVector
-    return Network
+    Network.forecast = forecast
+    return Network, weight
+
+if __name__ == '__main__':
+    from analysis.mkg import mkg_generator
+    SimulationOptions = simulation_options__(dt = 1e-3, T = 10,
+                                            contactMode = 'farthest',
+                                            electrodes = [72,29])
+
+    Connectivity = connectivity__(
+        filename = '2016-09-08-155153_asn_nw_00100_nj_00261_seed_042_avl_100.00_disp_10.00.mat')
+
+    # Connectivity = connectivity__(
+    #     filename = '2016-09-08-155044_asn_nw_00700_nj_14533_seed_042_avl_100.00_disp_10.00.mat')
+
+    tempStimulus = stimulus__(biasType = 'AC', 
+            TimeVector = SimulationOptions.TimeVector, 
+            onTime = 0, offTime = 20,
+            onAmp = 5, offAmp = 0.005)
+
+    # signal = mkg_generator(10000)
+    # signal = (np.sin(SimulationOptions.TimeVector))**2 + 2*(np.cos(SimulationOptions.TimeVector))**3
+
+    # tempStimulus = stimulus__(biasType = 'Custom', TimeVector = SimulationOptions.TimeVector, customSignal = signal)
+
+    # tempStimulus.signal = expander(tempStimulus.signal,2)
+    SimulationOptions.stimulus.append(tempStimulus)
+
+    tempStimulus = stimulus__(biasType = 'Drain', 
+            TimeVector = SimulationOptions.TimeVector)
+    SimulationOptions.stimulus.append(tempStimulus)
+
+    JunctionState = junctionState__(Connectivity.numOfJunctions, mode = 'tunneling')
+
+    sim1, weight = forecast_kev(SimulationOptions, Connectivity, JunctionState, 0.8, exclude_electrode = False, pre_activate = True)
