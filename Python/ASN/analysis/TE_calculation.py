4561c33cc03af71f7e3ec91fc96a30dda2b806b5
Ruomin Zhu <mcrzhu@gmail.com>
2019-11-19 10:48:04 +1100

    parameter changes

--- Python/ASN/analysis/TE_calculation.py
+++ Python/ASN/analysis/TE_calculation.py
@@ -39,13 +39,13 @@ def calc_network(Network, dt_sampling = 1e-1, N = 1e3, t_start=10, calculator = 
 if __name__ == '__main__': 
     sim1 = defaultSimulation(connectivity__(filename = '100nw_261junctions'), 
                             lite_mode = True,
-                            T = 210, dt = 1e-3, offTime = 10000000, 
+                            T = 2010, dt = 1e-3, offTime = 10000000, 
                             biasType = 'MKG', onAmp = 1, f = 1,
                             contactMode = 'preSet', electrodes = [72,29], 
                             junctionMode = 'binary', findFirst = False)
 
-    # dt_sample1 = [1e-3, 2e-3, 4e-3, 5e-3, 1e-2, 2e-2, 4e-2, 5e-2]
-    dt_sample1 = [1e-3, 2e-3]
+    dt_sample1 = [1e-3, 2e-3, 4e-3, 5e-3, 1e-2, 2e-2, 4e-2, 5e-2]
+    # dt_sample1 = [1e-3, 2e-3]
     dt_sample2 = [1e-1, 2e-1, 4e-1]
 
     sim2 = network__()
@@ -63,8 +63,8 @@ if __name__ == '__main__':
     TE40000 = []
 
     for i, this_dt in enumerate(dt_sample1):
-        TE20000.append(calc_network(sim1, this_dt, N = 200, t_start = 10, calculator = 'kraskov'))
-        TE40000.append(calc_network(sim2, this_dt, N = 400, t_start = 10, calculator = 'kraskov'))
+        TE20000.append(calc_network(sim1, this_dt, N = 20000, t_start = 10, calculator = 'kraskov'))
+        TE40000.append(calc_network(sim2, this_dt, N = 40000, t_start = 10, calculator = 'kraskov'))
     
     with open('data/TE_N20000.pkl', 'wb') as handle:
         pickle.dump(TE20000, handle, protocol = pickle.HIGHEST_PROTOCOL)
